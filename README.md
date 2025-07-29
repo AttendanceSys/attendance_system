b503ef4e2ba8196be24c4dd380775c96d7603ba4
qaalicabdi <qaaliladan437@gmail.com>
2025-07-29 17:50:47 -0400

    Initial commit with Flutter boilerplate

---
+++ README.md
@@ -0,0 +1,34 @@
+# My Flutter Project
+
+This is a reusable Flutter starter project for you all.
+
+## 🔧 How to run
+
+
+1. Get packages:
+
+    flutter pub get
+2. Run
+    flutter run
+3. clean
+    flutter clean
+
+4. this is how to Push it to GitHub
+
+# 1. Init git repo
+git init
+
+# 2. Add all project files
+git add .
+
+# 3. Commit the first version
+git commit -m "Initial commit with Flutter boilerplate"
+
+# 4. Rename default branch to main
+git branch -M main
+
+# 5. Add remote repository (your GitHub repo)
+git remote add origin https://github.com/AttendanceSys/attendance_system.git
+
+# 6. Push to GitHub
+git push -u origin main
---
+++ android/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.attendance_system"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.2.12479018" // ✅ Directly set the correct NDK version
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.attendance_system"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+
+flutter {
+    source = "../.."
+}
