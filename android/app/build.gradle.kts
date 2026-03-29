4941a644e78512db483b5244d1f9823cc155d8b7
qaalicabdi <qaaliladan437@gmail.com>
2026-03-29 14:26:47 +0300

    ...

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.attendance_system"
-        minSdk = 23
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
